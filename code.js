9fe8c094232585b5ef5672ee797a27d47d471fc9
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-03-02 19:22:05 -0500

    Create logic to allow calculation of complex math
    Two lists, nums and operators, are used in order to store and
    manipulate inputted numbers and operations. Code is added in order to
    allow the calculator to solve equations with several operations all in
    one.

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
+// Define the variables that store the numbers and operators
 let nums = []
+let operators = []
+// Variable that keeps track when to clear output. Used in 
+// long/complex equations
+let clearFirst = false
+
+// Select box that displays answer. Will be used multiple times
+// Throughout the project
+let output = document.querySelector('.output');
 
 const add = (a, b) => a + b;
 const sub = (a, b) => a - b;
@@ -19,19 +28,31 @@ const operate = (op, a, b) => {
     }
 }
 
-let output = document.querySelector('.output');
 function display(n) {
     output.textContent += n;
 }
 
 let num = document.querySelectorAll('.num');
-num.forEach(num => num.addEventListener('click', () => display(num.textContent)));
+num.forEach(num => num.addEventListener('click', () => {
+    if (clearFirst == true) output.textContent = '';
+    display(num.textContent)
+}));
 
 let clear = document.querySelector('.clear');
 clear.addEventListener('click', () => output.textContent = '');
 
 let ops = document.querySelectorAll('.op');
 ops.forEach(op => op.addEventListener('click', () => {
-    nums.push(output.textContent);
+    operators.push(op.textContent)
+    nums.push(parseInt(output.textContent));
     output.textContent = '';
+    if (nums.length >= 2) {
+        output.textContent = operate(operators[0], nums[0], nums[1]);
+        operators.splice(0, 1);
+        nums.splice(0, 2);
+        nums.push(parseInt(output.textContent));
+        clearFirst = true;
+    }
+    console.log(nums);
+    console.log(operators);
 }))
